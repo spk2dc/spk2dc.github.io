9bd0572cb3d7132a5b7e33a20aef9b7731e10728
Senthil <spk2dc@virginia.edu>
2020-08-05 13:06:12 -0400

    switching back to no basename and only env variable urls

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -14,9 +14,17 @@ function App() {
       <NavBar />
       <Footer />
       <Switch>
-        <Route exact path={`/`} component={Home} />
-        <Route exact path={`/projects`} component={Projects} />
-        <Route exact path={`/resume`} component={Resume} />
+        <Route exact path={`${process.env.PUBLIC_URL}/`} component={Home} />
+        <Route
+          exact
+          path={`${process.env.PUBLIC_URL}/projects`}
+          component={Projects}
+        />
+        <Route
+          exact
+          path={`${process.env.PUBLIC_URL}/resume`}
+          component={Resume}
+        />
         <Route component={Error} />
       </Switch>
     </div>
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename='/portfolio'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
