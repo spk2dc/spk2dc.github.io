90c1405b3ca152bf5e26b5807c2154c5b9d8dea5
Senthil <spk2dc@virginia.edu>
2020-08-05 01:23:38 -0400

    adding footer

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
+      <Footer />
     </div>
   );
 }
