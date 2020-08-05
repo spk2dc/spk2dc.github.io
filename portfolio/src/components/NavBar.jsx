8d7358d014222bc0a8aa358c136b8c69c215f258
Senthil <spk2dc@virginia.edu>
2020-08-05 11:56:43 -0400

    adding baseurl to beginning of page paths in navbar

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -4,10 +4,10 @@ import { Nav, Navbar } from "react-bootstrap";
 export default function NavBar() {
   return (
     <Navbar bg='dark' variant='dark'>
-      <Navbar.Brand href='/'>Senthil</Navbar.Brand>
+      <Navbar.Brand href='/portfolio'>Senthil</Navbar.Brand>
       <Nav className='mr-auto'>
-        <Nav.Link href='/projects'>Projects</Nav.Link>
-        <Nav.Link href='/resume'>Resume</Nav.Link>
+        <Nav.Link href='/portfolio/projects'>Projects</Nav.Link>
+        <Nav.Link href='/portfolio/resume'>Resume</Nav.Link>
       </Nav>
     </Navbar>
   );
