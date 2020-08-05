70ef543935d2ce2ee3ff1500e47a9789891b945f
Senthil <spk2dc@virginia.edu>
2020-08-05 01:14:16 -0400

    adding navbar

---
+++ portfolio/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+
+export default function NavBar() {
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Navbar.Brand href='/'>Senthil</Navbar.Brand>
+      <Nav className='mr-auto'>
+        <Nav.Link href='/projects'>Projects</Nav.Link>
+        <Nav.Link href='/resume'>Resume</Nav.Link>
+      </Nav>
+    </Navbar>
+  );
+}
