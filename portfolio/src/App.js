16643d9cfa0b0053799d575342da09057b1dbcab
Senthil <spk2dc@virginia.edu>
2020-08-05 12:23:04 -0400

    removing most of the new imports and components, working locally now

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,27 +8,20 @@ import Projects from "./components/projects/Projects";
 import Resume from "./components/Resume";
 import Error from "./components/Error";
 import { BrowserRouter } from "react-router-dom";
-import createHistory from "history/createBrowserHistory";
-import configureStore from "./store/configure-store";
-import { Provider } from "react-redux";
 
 function App() {
-  const store = configureStore({ history });
-
   return (
     <div className='App'>
-      <Provider store={store}>
-        <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
-          <NavBar />
-          <Footer />
-          <Switch>
-            <Route exact path='/portfolio' component={Home} />
-            <Route exact path='/portfolio/projects' component={Projects} />
-            <Route exact path='/portfolio/resume' component={Resume} />
-            <Route component={Error} />
-          </Switch>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter basename='/portfolio'>
+        <NavBar />
+        <Footer />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/projects' component={Projects} />
+          <Route exact path='/resume' component={Resume} />
+          <Route component={Error} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
