8035a44ddec7f571780255fbdb94499e8026abf2
Senthil <spk2dc@virginia.edu>
2020-08-30 17:03:18 -0400

    adding border to resume picture

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -33,7 +33,7 @@ const ResumePage = ({ data }, location) => {
             </a>
           </div>
 
-          <figure className="kg-card kg-image-card">
+          <figure className="kg-card kg-image-card border border-dark">
             <Img
               fluid={data.resumePic.childImageSharp.fluid}
               className="kg-image"
