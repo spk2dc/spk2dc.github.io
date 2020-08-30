ef46a712ba43c971fa6678ae1b244ae41730040c
Senthil <spk2dc@virginia.edu>
2020-08-30 00:41:23 -0400

    background color added when hovering on project post tile

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -8,14 +8,15 @@ export default props => (
     } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
     style={
       props.node.frontmatter.thumbnail && {
-        backgroundImage: `url(${
-          props.node.frontmatter.thumbnail.childImageSharp.fluid.src
-        })`,
+        backgroundImage: `url(${props.node.frontmatter.thumbnail.childImageSharp.fluid.src})`,
       }
     }
   >
     <Link to={props.node.fields.slug} className="post-card-link">
-      <div className="post-card-content">
+      <div
+        className="post-card-content"
+        style={{ backgroundColor: `#79aeffc7` }}
+      >
         <h2 className="post-card-title">
           {props.node.frontmatter.title || props.node.fields.slug}
         </h2>
