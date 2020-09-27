ab39566e95f3e179e53f3745fa1e72ba1d6d0f82
Senthil <spk2dc@virginia.edu>
2020-09-27 16:13:29 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,10 +26,10 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3adb20409880d7a73d9f.js"
+    "url": "webpack-runtime-21df2ffd8e7cda2716be.js"
   },
   {
-    "url": "styles.ba580f7b9793abf77799.css"
+    "url": "styles.9a0c3d3b0b2c5b6b82e3.css"
   },
   {
     "url": "styles-2d82ac8e3afc0c213061.js"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "96e2bb50e0cb471893fbfa9bd3a63cbd"
+    "revision": "0d28a91a5444af7c339d97fe0e0b1841"
   },
   {
     "url": "polyfill-98c1c53b2c5b5eb2390e.js"
