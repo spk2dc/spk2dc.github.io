7f8f63d889ca983f32a45a9fc8e989f64add0fef
Senthil <spk2dc@virginia.edu>
2020-09-26 21:13:25 -0400

    adding lightbox component for each image and click event with state to activate it

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,8 +1,10 @@
 import Img from "gatsby-image"
-import React from "react"
+import React, { useState } from "react"
 import Lightbox from "react-image-lightbox"
 
 const Gallery = ({ images, itemsPerRow }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   function chunk(array, groupSize) {
     const groups = []
     for (let i = 0; i < array.length; i += groupSize) {
@@ -27,15 +29,30 @@ const Gallery = ({ images, itemsPerRow }) => {
         const rowAspectRatioSum = sum(row.map(image => image.aspectRatio))
 
         return row.map(image => (
-          <Img
-            key={image.src}
-            className="gallery-image-container"
-            fluid={image}
-            title={image.caption}
-            style={{
-              width: `${(image.aspectRatio / rowAspectRatioSum) * 100}%`,
+          <div
+            className="clickTest"
+            onClick={e => {
+              console.log("lightbox test", e.target)
+              setIsOpen(true)
             }}
-          />
+          >
+            <Img
+              key={image.src}
+              className="gallery-image-container"
+              fluid={image}
+              title={image.caption}
+              style={{
+                width: `${(image.aspectRatio / rowAspectRatioSum) * 100}%`,
+              }}
+            />
+
+            {isOpen && (
+              <Lightbox
+                mainSrc={image.src}
+                onCloseRequest={() => setIsOpen(false)}
+              />
+            )}
+          </div>
         ))
       })}
     </div>
