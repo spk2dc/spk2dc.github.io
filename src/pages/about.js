87e81054e98415648f9811862cf183363796413a
Senthil <spk2dc@virginia.edu>
2020-10-15 19:49:21 -0400

    Revert "using core get input to because workflow is storing secrets in with instead of env now"
    This reverts commit 7debfb0a0e5fd1d05b8ac45a102c3df483f34432.
    
    commands used:
    git revert --no-commit 92d3af855a95e0db134c317eb38cfbd2d1e1c269..HEAD
    git commit
    
    source:
    https://stackoverflow.com/questions/4114095/how-do-i-revert-a-git-repository-to-a-previous-commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,6 @@ import SEO from "../components/seo"
 
 import "../utils/normalize.css"
 import "../utils/css/screen.css"
-const core = require("@actions/core")
 
 const AboutPage = ({ data }, location) => {
   const siteTitle = data.site.siteMetadata.title
@@ -24,23 +23,13 @@ const AboutPage = ({ data }, location) => {
           </h2>
 
           <p>
-            GATSBY_GOOGLE_ANALYTICS_ID:{" "}
-            {core.getInput("GATSBY_GOOGLE_ANALYTICS_ID")
-            //process.env.GATSBY_GOOGLE_ANALYTICS_ID
-            }
+            GATSBY_GOOGLE_ANALYTICS_ID: {process.env.GATSBY_GOOGLE_ANALYTICS_ID}
           </p>
           <p>
             REACT_APP_GOOGLE_ANALYTICS_ID:&nbsp;
-            {core.getInput("REACT_APP_GOOGLE_ANALYTICS_ID")
-            //process.env.REACT_APP_GOOGLE_ANALYTICS_ID
-            }
+            {process.env.REACT_APP_GOOGLE_ANALYTICS_ID}
           </p>
-          <p>
-            GOOGLE_ANALYTICS_ID:{" "}
-            {core.getInput("GOOGLE_ANALYTICS_ID")
-            //process.env.GOOGLE_ANALYTICS_ID
-            }
-          </p>
+          <p>GOOGLE_ANALYTICS_ID: {process.env.GOOGLE_ANALYTICS_ID}</p>
 
           <figure className="kg-card kg-image-card kg-width-full">
             <Img
