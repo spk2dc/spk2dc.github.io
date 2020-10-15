a2de667b61684864d8e63a0bf2275e8673a4d2a7
GitHub Action <action@github.com>
2020-10-15 23:55:36 +0000

    Add changes

--- siteConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "Senthil's Portfolio",
-  shortName: "Portfolio",
-  description: "Senthil Kannan's portfolio - built with Gatsby",
-  url: "https://spk2dc.github.io/", // Domain of your site without prefix!
-  prefix: "/",
-  author: "Senthil Kannan",
-  twitter: "null",
-}
